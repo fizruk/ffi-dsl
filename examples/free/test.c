20bff0ca2e01f3ab6806a2ee2e3e1fb4050d48bf
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-09-28 09:13:31 +0400

    test.c comment

--- examples/free/test.c
+++ examples/free/test.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "test.h"
 
+/* This is equivallent to this code in Haskell:
+ *
+ * test :: DSL ()
+ * test = do
+ *    output "Hello, What's your name?"
+ *    name <- input
+ *    output $ "Goodbye, " ++ name ++ "!"
+ *
+ * */
 void test(HsStablePtr m) {
   char *name;
   char *bye;
