a93aab72d8c34f572c140a55e775257bdddd9a40
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-09-28 08:24:21 +0400

    use library functions in example

--- examples/free/DSLFFI.hs
+++ examples/free/DSLFFI.hs
@@ -1,45 +1,23 @@
 module DSLFFI where
 
 import Foreign.C
-import Foreign.StablePtr
+import Foreign.DSL
 
 import DSL (DSL)
 import qualified DSL as D
 
-import Data.IVar.Simple (IVar)
-import qualified Data.IVar.Simple as IVar
-
-import Control.Concurrent.STM
 import Control.Monad.Free
 
-import System.IO.Unsafe
+foreign export ccall output :: DSLChunks DSL -> CString -> IO ()
+foreign export ccall input  :: DSLChunks DSL -> IO CString
 
-type DSLChanPtr = StablePtr (TChan (DSL ()))
-
-foreign export ccall output :: DSLChanPtr -> CString -> IO ()
-foreign export ccall input  :: DSLChanPtr -> IO CString
-
-output :: DSLChanPtr -> CString -> IO ()
+output :: DSLChunks DSL -> CString -> IO ()
 output m cs = do
   s <- peekCString cs
   genDSL_ m $ D.output s
 
-input :: DSLChanPtr -> IO CString
+input :: DSLChunks DSL -> IO CString
 input m = do
   s <- genDSL m D.input
   newCString s
-
-genDSL :: DSLChanPtr -> DSL a -> IO a
-genDSL ptr m = do
-  ivar <- IVar.new
-  chan <- deRefStablePtr ptr
-  atomically . writeTChan chan $ do
-    res <- m
-    return $! (unsafePerformIO $ IVar.write ivar res)
-  return $ IVar.read ivar
-
-genDSL_ :: DSLChanPtr -> DSL a -> IO ()
-genDSL_ ptr m = do
-  chan <- deRefStablePtr ptr
-  atomically . writeTChan chan $ m >> return ()
 
--- examples/free/Main.hs
+++ examples/free/Main.hs
@@ -1,39 +1,15 @@
 module Main where
 
 import Foreign.C
-import Foreign.StablePtr
+import Foreign.DSL
 
 import DSL (DSL)
 import qualified DSL as D
 
-import Control.Concurrent.STM
-import Control.Monad.Parallel (forkExec)
-
-import Data.IVar.Simple
-
-import Control.Monad
-import Control.Monad.Free
-
-import System.IO.Unsafe
-
 foreign import ccall "test.h test"
-  test_c :: StablePtr (TChan (DSL ())) -> IO ()
-
-test :: IO (DSL ())
-test = do
-  chan <- atomically newTChan
-  ptr  <- newStablePtr chan
-  mt <- forkExec $ test_c ptr
-  mc <- forkExec $ readDSL chan
-  unsafeInterleaveIO mt
-  mc
-  where
-    readDSL chan = do
-      stmt <- atomically $ readTChan chan
-      rest <- unsafeInterleaveIO $ readDSL chan
-      return $ stmt >> rest
+  test_c :: DSLChunks DSL -> IO ()
 
 main :: IO ()
 main = do
-  prg <- test
+  prg <- runForeignDSL test_c
   D.runDSL prg
